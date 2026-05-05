a2f4c67a94cf5c20de8f72820f1ef10a532c5e24
Joseph-ask <joejoe.a.2005@gmail.com>
2026-05-05 16:47:25 +0100

    Added second constructor and made populate function private

--- Assignment-2/include/sklearn_cpp/linear_model/utils.hpp
+++ Assignment-2/include/sklearn_cpp/linear_model/utils.hpp
@@ -20,71 +20,76 @@ private:
         return (ss >> d) && !(ss >> c);
     }
 
+void populate(const std::string& filename)
+{
+    std::ifstream file(filename);
+
+    if (!file.is_open())
+    {
+        throw std::runtime_error("Could not open file");
+    }
+
+    std::string line;
+    bool firstLine = true;
+
+    while (std::getline(file, line))
+    {
+        std::stringstream ss(line);
+        std::string value;
+
+        std::vector<std::string> raw;
+        std::vector<double> row;
+
+        while (std::getline(ss, value, ','))
+        {
+            raw.push_back(value);
+        }
+
+        // detect header
+        if (firstLine)
+        {
+            bool header = false;
+            for (const auto& v : raw)
+            {
+                if (!isNumber(v))
+                {
+                     
+                    header = true;
+                    break;
+                }
+            }
+
+            firstLine = false;
+
+            if (header)
+                continue;
+        }
+
+        for (const auto& v : raw)
+        {
+            if (!v.empty())
+                row.push_back(std::stod(v));
+        }
+
+        data.push_back(row);
+    }
+}
+
+
 public:
-    // Constructor 
+    // Constructor for file name
     Dataset(const std::string& filename)
     {
         populate(filename);
     }
 
-    void populate(const std::string& filename)
-    {
-        std::ifstream file(filename);
-
-        if (!file.is_open())
-        {
-            throw std::runtime_error("Could not open file");
-        }
-
-        std::string line;
-        bool firstLine = true;
-
-        while (std::getline(file, line))
-        {
-            std::stringstream ss(line);
-            std::string value;
-
-            std::vector<std::string> raw;
-            std::vector<double> row;
-
-            while (std::getline(ss, value, ','))
-            {
-                raw.push_back(value);
-            }
-
-            // detect header
-            if (firstLine)
-            {
-                bool header = false;
-                for (const auto& v : raw)
-                {
-                    if (!isNumber(v))
-                    {
-                        header = true;
-                        break;
-                    }
-                }
-
-                firstLine = false;
-
-                if (header)
-                    continue;
-            }
-
-            for (const auto& v : raw)
-            {
-                if (!v.empty())
-                    row.push_back(std::stod(v));
-            }
-
-            data.push_back(row);
-        }
-    }
+    // Constructor for vector of vectors
+    Dataset(const std::vector<std::vector<double>>& data): data{data} {}
 
     //Allows user to use data
     const std::vector<std::vector<double>>& getData() const{ return data; }
 
-    //Allows user to print data a certain amount of data
+    //Allows user to print data
     void print() const{
     for (const auto& row : data){
         for (const auto& value : row){
