b5426668f22fe4fd05846781a6921c1ce9c37b99
Joseph-ask <joejoe.a.2005@gmail.com>
2026-05-12 15:05:16 +0100

    Fully validated R_squared function using test.cpp (:

--- Assignment-2/examples/test.cpp
+++ Assignment-2/examples/test.cpp
@@ -3,12 +3,8 @@
 int main(){
     const Dataset test{"/home/joseph/University/AP/Ap-A2/data/concrete.csv"};
 
-    // std::vector<double> y = {3, -0.5, 2, 7};
-    // std::vector<double> y2 = {2.5, 0.0, 2, 8};
-
-    std::vector<double> y{1.2,2,3};
-    std::vector<double> y2{1,2,3};
-
+    std::vector<double> y = {3, -0.5, 2, 7};
+    std::vector<double> y2 = {2.5, 0.0, 2, 8};
     double r2 = R_squared(y, y2);
 
     std::cout << "R^2 = " << r2 << std::endl;
